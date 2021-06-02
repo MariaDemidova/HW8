93d18c62dcad86cd998ae24c448ff774828b8f2c
MariaDemidova <demidova.ma@yandex.ru>
2021-06-02 15:23:16 +0300

    new Judge

--- src/demidova/Judge.java
+++ src/demidova/Judge.java
@@ -7,15 +7,16 @@ import demidova.runnersJampers.RunnerAndJumper;
 
 public class Judge {
 
-    public static void sayResult(RunnerAndJumper runner, Obstacle track) {
-        if(isSuccess(runner, track)) System.out.printf("%s успешно преодолел препятствие.\n", runner.getName());
-        else System.out.printf("%s не осилил препятствие.\n", runner.getName());
+    public static void sayResult(RunnerAndJumper runnerAndJumper, Obstacle track) {
+        if(isSuccess(runnerAndJumper, track)) System.out.printf("%s успешно преодолел препятствие.\n", runnerAndJumper.getName());
+        else System.out.printf("%s не осилил препятствие.\n", runnerAndJumper.getName());
     }
 
     private static boolean isSuccess(RunnerAndJumper runnerAndJumper, Obstacle obstacle) {
         if (obstacle instanceof Wall && obstacle.getValue() < runnerAndJumper.getMaxHeightToJump())
             return true;
-        else if(obstacle instanceof Track && obstacle.getValue() < runnerAndJumper.getMaxLengthToRun()) return true;
+        else if(obstacle instanceof Track && obstacle.getValue() < runnerAndJumper.getMaxLengthToRun())
+            return true;
         else return false;
     }
 }
